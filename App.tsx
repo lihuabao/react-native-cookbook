3a314aadb3a6cc1564f8f6c0689de3b4c1c977b4
Lihua <lihua.lin@thoughtworks.com>
2020-03-23 19:12:41 +0100

    init commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>I just wanna test you out :D</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
---
+++ README.md
@@ -0,0 +1,9 @@
+# react-cookbook 🥗🥘🧁
+
+install expo cli `npm install -g expo-cli` to creat template quickly
+
+install Xcode via AppStore to use simulator
+
+run `expo start` to see app in simulator
+
+download echo client app on phone/simulator
