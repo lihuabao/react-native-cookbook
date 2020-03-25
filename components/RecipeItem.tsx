b2bcbe7d7c8461bcfe68c38ba284cfae90dc23cf
Mekong Lam <meki@Mekongs-MacBook-Pro.local>
2020-03-25 11:33:38 +0100

    Lihua/Meki - Add image

--- components/RecipeItem.tsx
+++ components/RecipeItem.tsx
@@ -6,6 +6,7 @@ import { Text, View, Image } from "react-native";
 export default function RecipeItem () {
     return (
     <View>
+      <Image source={require('../assets/pizzaPepperoni.jpg')} style = {{ width: 200, height: 200 }}/>
       <Text>Apfelstrudel</Text>
       <Text>30 Minutes</Text>
     </View>
