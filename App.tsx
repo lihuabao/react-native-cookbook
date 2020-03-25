dd2211cd4e400a14c3c5199d034889d8a60dd0c3
Lihua <lihua.lin@thoughtworks.com>
2020-03-25 12:05:55 +0100

    add styles to RecipeItem and App

--- App.tsx
+++ App.tsx
@@ -3,16 +3,19 @@ import { StyleSheet, Text, View } from "react-native";
 import RecipeItem from "./components/RecipeItem";
 
 export default function App() {
-   return <RecipeItem/>;
+  return (
+    <View style={styles.container}>
+      <RecipeItem />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#aaa",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    marginVertical: 30
   }
 });
-
-
--- components/RecipeItem.tsx
+++ components/RecipeItem.tsx
@@ -1,14 +1,31 @@
-import React, { Component } from 'react'
-import { Text, View, Image } from "react-native";
+import React, { Component } from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderColor: "#000",
+    borderWidth: 1,
+    height: 60,
+    width: "100%"
+  },
+  img: { width: 50, height: 50, margin: 15 },
+  textContainer: { margin: 10 }
+});
 
-
-export default function RecipeItem () {
-    return (
-    <View>
-      <Image source={require('../assets/pizzaPepperoni.jpg')} style = {{ width: 200, height: 200 }}/>
-      <Text>Apfelstrudel</Text>
-      <Text>30 Minutes</Text>
+export default function RecipeItem() {
+  return (
+    <View style={styles.wrapper}>
+      <Image
+        source={require("../assets/pizzaPepperoni.jpg")}
+        style={styles.img}
+      />
+      <View>
+        <Text>Apfelstrudel</Text>
+        <Text>30 Minutes</Text>
+      </View>
     </View>
   );
-  }
+}
