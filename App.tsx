77d78e40e80519837b9205d8b651b5b6a4e6af93
Lihua <lihua.lin@thoughtworks.com>
2020-03-31 10:47:18 +0200

    add stack navigator and define home screen

--- App.tsx
+++ App.tsx
@@ -2,10 +2,21 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import RecipeList from "./components/RecipeList";
 import withFeaureToggle from "./withFeatureToggle";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const FinalComponent = withFeaureToggle(RecipeList, "RecipeList");
-  return <FinalComponent />;
+  // return <FinalComponent />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={FinalComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
