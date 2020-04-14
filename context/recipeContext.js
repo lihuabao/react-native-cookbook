b303aa4cdbde6e38ae4ccab2094f965278cd1289
Lihua <lihua.lin@thoughtworks.com>
2020-04-14 11:22:24 +0200

    add useReducer hook for different actions (add, remove, update)

--- context/recipeContext.js
+++ context/recipeContext.js
@@ -1,10 +1,11 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useReducer } from "react";
 import recipeData from "../recipeData.json";
 
 export const RecipeContext = createContext();
 
 const RecipeProvider = ({ children }) => {
-  const [recipes, setRecipes] = useState(recipeData.recipes);
+  const [existingRecipes, setRecipes] = useState(recipeData.recipes);
+
   const saveRecipe = (recipe) => {
     const newRecipe = {
       id: Math.random().toString(), // not really unique but it's just an example
@@ -14,10 +15,29 @@ const RecipeProvider = ({ children }) => {
       ingredients: recipe.ingredients,
       steps: recipe.steps,
     };
-    setRecipes([newRecipe, ...recipes]);
+    setRecipes([newRecipe, ...existingRecipes]);
   };
+
+  const [recipes, dispatch] = useReducer((recipes, { type, obj }) => {
+    switch (type) {
+      case "add":
+        return [...recipes, obj];
+      case "update":
+        return recipes.map((recipe) => {
+          if (recipe.id == obj.id) {
+            recipe = obj;
+          }
+          return recipe;
+        });
+      case "remove":
+        return recipes.filter((_, index) => index !== value);
+      default:
+        return recipes;
+    }
+  }, recipeData.recipes);
+
   return (
-    <RecipeContext.Provider value={{ recipes, saveRecipe }}>
+    <RecipeContext.Provider value={{ recipes, dispatch }}>
       {children}
     </RecipeContext.Provider>
   );
