70e47b1f6acacda1e98cf67c38c21a65ed122820
Lihua <lihua.lin@thoughtworks.com>
2020-04-17 11:58:47 +0200

    remove ugly borders

--- components/RecipeItem.tsx
+++ components/RecipeItem.tsx
@@ -31,8 +31,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    borderColor: "black",
-    borderWidth: 1,
     maxWidth: "50%",
     height: Dimensions.get("screen").width / 2,
   },
