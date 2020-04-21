4e66ab1b01fbda1280c1dda72d75c58b561825e6
KatharinaWuerth <K.Wuerth@gmx.net>
2020-04-21 11:28:44 +0200

    Add swipe to delete for ingredients in create view

--- components/RecipeCreateView.tsx
+++ components/RecipeCreateView.tsx
@@ -5,9 +5,11 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  ScrollView
 } from "react-native";
 import { RecipeContext } from "../context/recipeContext";
 import { lettersOnly, numbersOnly } from "../helpers.js";
+import Swipeout from "react-native-swipeout";
 
 export default function RecipeCreateView(props) {
   const { dispatch } = useContext(RecipeContext);
@@ -42,8 +44,8 @@ export default function RecipeCreateView(props) {
         name: name,
         minutes: minutes,
         ingredients: ingredientList,
-        steps: stepList,
-      },
+        steps: stepList
+      }
     });
   };
 
@@ -52,140 +54,164 @@ export default function RecipeCreateView(props) {
     props.navigation.navigate("Recipes");
   };
 
+  const deletIngredient = name => {
+    const updatedList = ingredientList.filter(i => i.name !== name);
+    setIngredientList(updatedList);
+  };
+  const renderIngredientItem = ingredient => {
+    let swipeBtns = [
+      {
+        text: "Delete",
+        backgroundColor: "red",
+        underlayColor: "rgba(0, 0, 0, 1, 0.6)",
+        onPress: () => {
+          deletIngredient(ingredient.name);
+        }
+      }
+    ];
+
+    return (
+      <Swipeout
+        right={swipeBtns}
+        autoClose="true"
+        backgroundColor="transparent"
+      >
+        <TouchableOpacity style={styles.ingredientItem} key={ingredient.name}>
+          <Text
+            style={{ justifyContent: "center" }}
+          >{`\u2022 ${ingredient.qty} ${ingredient.name} \n`}</Text>
+        </TouchableOpacity>
+      </Swipeout>
+    );
+  };
   return (
-    <View style={styles.container}>
-      <View style={styles.inputWrap}>
-        <Text style={styles.inputTitle}>Name:</Text>
-        <TextInput
-          value={name}
-          style={styles.input}
-          onChangeText={(text) => setName(lettersOnly(text))}
-          maxLength={25}
-        />
-      </View>
-      <View style={styles.inputWrap}>
-        <Text style={styles.inputTitle}>Time:</Text>
-        <View style={styles.row}>
-          <View style={styles.inlineWrap}>
-            <TextInput
-              keyboardType="numeric"
-              value={minutes}
-              style={styles.inlineInput}
-              maxLength={3}
-              onChangeText={(input) =>
-                (numbersOnly(input) || input === "") && setMinutes(input)
-              }
-            />
-            <Text>min</Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.inputWrap}>
+          <Text style={styles.inputTitle}>Name:</Text>
+          <TextInput
+            value={name}
+            style={styles.input}
+            onChangeText={text => setName(lettersOnly(text))}
+            maxLength={25}
+          />
+        </View>
+        <View style={styles.inputWrap}>
+          <Text style={styles.inputTitle}>Time:</Text>
+          <View style={styles.row}>
+            <View style={styles.inlineWrap}>
+              <TextInput
+                keyboardType="numeric"
+                value={minutes}
+                style={styles.inlineInput}
+                maxLength={3}
+                onChangeText={input =>
+                  (numbersOnly(input) || input === "") && setMinutes(input)
+                }
+              />
+              <Text>min</Text>
+            </View>
           </View>
         </View>
-      </View>
-      <View style={styles.inputWrap}>
-        <Text style={styles.inputTitle}>Ingredients:</Text>
-
-        {ingredientList.map((ingredient) => (
-          <Text key={ingredient.name}>
-            {`\u2022 ${ingredient.qty} ${ingredient.name} \n`}
-          </Text>
-        ))}
-
-        <View style={styles.row}>
+        <View style={styles.inputWrap}>
+          <Text style={styles.inputTitle}>Ingredients:</Text>
+          {ingredientList.map(ingredient => renderIngredientItem(ingredient))}
+          <View style={styles.row}>
+            <TextInput
+              value={ingredient}
+              style={styles.inlineInput}
+              onChangeText={text => setIngredient(text)}
+            />
+            <Text>Qty:</Text>
+            <TextInput
+              value={amount}
+              style={styles.inlineInput}
+              onChangeText={text => setAmount(text)}
+            />
+          </View>
+          <TouchableOpacity style={styles.addButton} onPress={addIngredient}>
+            <Text>Add an ingredient</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.inputWrap}>
+          <Text style={styles.inputTitle}>Steps:</Text>
+          {stepList.map((step, index) => (
+            <Text key={index.toString()}>{`${index + 1}. ${step} \n`}</Text>
+          ))}
           <TextInput
-            value={ingredient}
-            style={styles.inlineInput}
-            onChangeText={(text) => setIngredient(text)}
-          />
-          <Text>Qty:</Text>
-          <TextInput
-            value={amount}
-            style={styles.inlineInput}
-            onChangeText={(text) => setAmount(text)}
+            multiline
+            value={step}
+            style={[styles.input, { height: Math.max(35, multilineHeight) }]}
+            onChangeText={text => setStep(text)}
+            onContentSizeChange={event => {
+              setMultiplineHeight(event.nativeEvent.contentSize.height);
+            }}
           />
         </View>
-        <TouchableOpacity style={styles.addButton} onPress={addIngredient}>
-          <Text>Add an ingredient</Text>
+        <TouchableOpacity style={styles.addButton} onPress={addStep}>
+          <Text>Add a step</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.saveButton} onPress={onSaveRecipe}>
+          <Text>Save</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.inputWrap}>
-        <Text style={styles.inputTitle}>Steps:</Text>
-
-        {stepList.map((step, index) => (
-          <Text key={index.toString()}>{`${index + 1}. ${step} \n`}</Text>
-        ))}
-
-        <TextInput
-          multiline
-          value={step}
-          style={[styles.input, { height: Math.max(35, multilineHeight) }]}
-          onChangeText={(text) => setStep(text)}
-          onContentSizeChange={(event) => {
-            setMultiplineHeight(event.nativeEvent.contentSize.height);
-          }}
-        />
-      </View>
-      <TouchableOpacity style={styles.addButton} onPress={addStep}>
-        <Text>Add a step</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.saveButton} onPress={onSaveRecipe}>
-        <Text>Save</Text>
-      </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    margin: 30,
+    margin: 30
   },
   inputWrap: {
-    marginVertical: 20,
+    marginVertical: 20
   },
   row: {
     flexDirection: "row",
-    marginVertical: 20,
+    marginVertical: 20
   },
   inlineWrap: {
     flex: 1,
     flexDirection: "row",
-    alignItems: "flex-end",
+    alignItems: "flex-end"
   },
   inlineInput: {
     flex: 1,
     borderColor: "black",
-    borderBottomWidth: 1,
+    borderBottomWidth: 1
   },
   inputTitle: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 10,
+    marginBottom: 10
   },
   input: {
     justifyContent: "flex-end",
     height: 30,
     borderColor: "black",
-    borderBottomWidth: 1,
+    borderBottomWidth: 1
   },
   multilineInput: {
     height: 100,
     borderColor: "black",
     borderWidth: 1,
-    marginTop: 10,
+    marginTop: 10
+  },
+  ingredientItem: {
+    borderBottomColor: "lightgrey",
+    borderBottomWidth: 1,
+    flexDirection: "column"
   },
   addButton: {
     alignItems: "center",
     padding: 10,
     marginVertical: 10,
-    backgroundColor: "peachpuff",
+    backgroundColor: "peachpuff"
   },
   saveButton: {
     alignItems: "center",
     backgroundColor: "sandybrown",
     padding: 10,
-    marginVertical: 10,
-  },
-  picker: {
-    height: 30,
-    width: 80,
-    alignSelf: "center",
-  },
+    marginVertical: 10
+  }
 });
